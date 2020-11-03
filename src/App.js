481318efa55a9b7dec5e48fd1c7ef43991cf7d46
DUONGNAMTRUONG <namtruongdev@gmail.com>
2020-11-03 14:55:00 +0700

    updaste

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
+
+import { reducer, initState } from './store/reducers/countReducer';
+import { increment, decrement } from './store/actions/countAction';
 import './App.css';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p>{state.count}</p>
+      <button onClick={() => dispatch(increment)}>Increment</button>
+      <button onClick={() => dispatch(decrement)}>Decrement</button>
     </div>
   );
 }
---
+++ src/store/actions/countAction.js
@@ -0,0 +1,9 @@
+import { INCREMENT, DECREMENT } from '../Contants';
+
+export const increment = {
+  type: INCREMENT,
+};
+
+export const decrement = {
+  type: DECREMENT,
+};
